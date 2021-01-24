cf13c3f28978937b207a3d90583cfbfaab050e63
melisagokmen <52598282+melisagokmen@users.noreply.github.com>
2021-01-24 15:23:57 +0300

    ArrayBlockingQueue ile producer consumer problemi eklendi

---
+++ producerConsumer/src/ProducerConsumer.java
@@ -0,0 +1,55 @@
+
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ProducerConsumer {
+    Random random=new Random();
+    BlockingQueue<Integer> queue=new ArrayBlockingQueue<Integer>(10);
+    public void produce(){
+        while(true){
+            try {
+                
+                Thread.sleep(1000);
+                
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ProducerConsumer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+         
+            try {
+                
+               Integer value=random.nextInt(100);
+               System.out.println("Producer üretiyor..."+value);
+               queue.put(value);
+               
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ProducerConsumer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    public void consume(){
+        while(true){
+            try {
+                
+                Thread.sleep(5000);
+                
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ProducerConsumer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+         
+            try {
+                
+               Integer value=queue.take();
+               System.out.println("Consumer tüketiyor..."+value);
+                System.out.println("Queue size: "+queue.size());
+              
+               
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ProducerConsumer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+}
